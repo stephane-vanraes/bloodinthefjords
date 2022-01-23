ed3ac9d9b8c8b9cef26d9833cbc15a5fc92101ce
stephane-vanraes <stephane.vanraes@gmail.com>
2022-01-23 15:40:05 +0100

    remove tolowercase

--- src/lib/marked/linkifier.js
+++ src/lib/marked/linkifier.js
@@ -17,7 +17,7 @@ export default {
         const type = token.link2[0]
         let name = token.link2.substring(1)
 
-        let url = name.toLowerCase().replaceAll(' ', '-')
+        let url = name.replaceAll(' ', '-')
 
         if (name.startsWith('-')) {
             name = name?.substring(1).split(' ')[0]

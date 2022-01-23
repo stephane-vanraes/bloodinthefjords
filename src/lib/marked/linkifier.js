1f635db61f917232b9fcd3ac683ef11c15101009
stephane-vanraes <stephane.vanraes@gmail.com>
2022-01-23 15:42:47 +0100

    bla

--- src/lib/marked/linkifier.js
+++ src/lib/marked/linkifier.js
@@ -15,9 +15,9 @@ export default {
     },
     renderer(token) {
         const type = token.link2[0]
-        let name = token.link2.substring(1) ?? ''
+        let name = token.link2.substring(1)
 
-        let url = name.toLowerCase().replaceAll(' ', '-')
+        let url = name//.toLowerCase().replaceAll(' ', '-')
 
         if (name.startsWith('-')) {
             name = name?.substring(1).split(' ')[0]
